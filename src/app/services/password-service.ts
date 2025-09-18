ba69c2909bbdc86a3041af034f7634da7137176a
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2025-09-18 20:49:40 +0300

    Add getAll function in password service

--- src/app/services/password-service.ts
+++ src/app/services/password-service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { ApiResponse } from '../models/ApiResponse';
+import { Password } from '../models/Password';
 
 @Injectable({
   providedIn: 'root',
@@ -11,12 +12,24 @@ export class PasswordService {
 
   delete(id: string, token: string) {
     return this.http.delete<ApiResponse<null>>(
-      `${environment.msal.azureFunctionsUrls.deletePasswordFunc}?id=${id}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
+      environment.msal.azureFunctionsUrls.deletePasswordFunc,
+      this.getOptions(token, new HttpParams().append('id', id))
     );
+  }
+
+  getAll(token: string) {
+    return this.http.get<ApiResponse<Password[]>>(
+      environment.msal.azureFunctionsUrls.getAllPasswordFunc,
+      this.getOptions(token)
+    );
+  }
+
+  private getOptions(token: string, params?: HttpParams) {
+    return {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: params,
+    };
   }
 }
